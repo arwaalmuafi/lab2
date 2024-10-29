0867a4b4e312ca5e37f9e8d4555d34a47a4c3eee
arwaalmuafi <arwaalmaufi@gmail.com>
2024-10-29 19:39:09 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,20 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-         /* 8. Create a method that generates the Fibonacci sequence up to a specified number of terms.
-          Hint: The Fibonacci sequence is a mathematical sequence of numbers that starts with 0 and 1,
-           and each subsequent number in the sequence is the sum of the two preceding ones.
-         */
-        System.out.print("Enter the number of Fibonacci terms to generate: ");
-        int terms = input.nextInt();
-
-        System.out.println("Fibonacci sequence with " + terms + " terms:");
-        generateFibonacci(terms);
+        //4. Create a method to reverse an array of integers. Implement the method without creating a new array.
+        int[] array = {5, 4, 3, 2, 1};
+        System.out.println("Original Array: " + Arrays.toString(array));
+        System.out.println("reversed Array: " + Arrays.toString(reverseArray(array)));
     }
-    public static void generateFibonacci(int terms) {
-        if (terms <= 0) {
-            System.out.println("Please enter a positive number.");
-            return;
+
+    public static int[] reverseArray(int[] array) {
+        int i = 0, j = array.length - 1;
+        for (i = 0; i < array.length / 2; i++, j--) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
         }
-        int[] fibonacciSequence = new int[terms];
-        fibonacciSequence[0] = 0;
-        if (terms > 1) {
-            fibonacciSequence[1] = 1;
-        }
-        for (int i = 2; i < terms; i++) {
-            fibonacciSequence[i] = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
-        }
-        for (int i = 0; i < terms; i++) {
-            System.out.print(fibonacciSequence[i] + " ");
-        }
-        System.out.println();
+        return array;
     }
 }
